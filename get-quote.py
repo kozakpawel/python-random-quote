1e822127991180b5262d05329f2fdc89e6c82ce1
Szczesny <pawel.a.szczesny@capgemini.com>
2021-04-07 21:20:08 +0200

    adding new quotes to the file

--- get-quote.py
+++ get-quote.py
@@ -5,9 +5,12 @@ def primary():
   f = open("python-lab\python-random-quote\quotes.txt")
   quotes = f.readlines()
   f.close()
+  f = open("python-lab\python-random-quote\quotes.txt", "a")
+  f.write("Now the file has more content!")
+  f.close()
   last = len(quotes) - 1
   rnd = random.randint(0, last)
-  print(quotes[rnd])
+  print(quotes[rnd],quotes[rnd-1],end='')
 
 if __name__== "__main__":
   primary()
